3b797114c3a5ec7a5729f241970d6519c0e98948
Unknown <josef.devo@gmail.com>
2018-04-27 16:03:43 +0200

    Created Screen for userSettings!

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -5,6 +5,8 @@ $(function () {
     $("#chooseStudents").hide();
     $("#mavenForm").hide();
     $("#regretPanel").hide();
+    $("#testContainer").hide();
+    $("#teacherSettings").hide();
 
     /* Dölj varje div hanterar det nya testet! */
     $("#dropDownJava").click(function () {
@@ -25,4 +27,79 @@ $('.shareTestContainer').click(function () {
 
 $('.createTestContainer').click(function () {
     $("#testDetails").show();
+});
+
+$('.doTestContainer').click(function () {
+    $("#testContainer").show();
+});
+
+
+$('.tSettingsContainer').click(function () {
+    $('#tSettings').replaceWith(`
+    
+    <div id="teacherSettings">
+    <div class="teacherCard">
+        <div class="card-block">
+            <form>
+                <div class="form-group">
+                    <label class="userSettingsLabel">Update your user details</label>
+                    <hr>
+                    <label id="oldUserNameLabel" for="oldUserNameInput">Enter your old email address</label>
+                    <input required v-model="oldUserName" type="text" class="form-control" id="oldUserNameInput">
+                </div>
+                <div class="form-group">
+                    <label id="newUserNameLabel" for="newUserNameInput">Enter your new email address</label>
+                    <input required v-model="newUserName" type="text" class="form-control" id="newUserNameInput">
+                </div>
+                <div class="form-group">
+                    <label id="oldPasswordLabel" for="oldPasswordInput">Enter your old password</label>
+                    <input required v-model="oldPassword" type="text" class="form-control" id="oldPasswordInput"
+                           placeholder="welcome">
+                </div>
+                <div class="form-group">
+                    <label id="newPasswordLabel" for="newPasswordInput">Enter your old password</label>
+                    <input required v-model="newPassword" type="text" class="form-control" id="newPasswordInput"
+                           placeholder="welcome">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+    
+    `
+    );
+});
+
+$('.sSettingsContainer').click(function () {
+    $('#sSettings').replaceWith(`
+    
+<div id="studentSettings">
+    <div class="studentCard">
+        <div class="card-block">
+            <form class="form-create" action="/" method="post">
+                <div class="form-group">
+                    <label class="userSettingsLabel">Update your user details</label>
+                    <hr>
+                    <label id="oldUserNameLabel" for="oldUserNameInput">Enter your old email address</label>
+                    <input required v-model="oldUserName" type="text" class="form-control" id="oldUserNameInput">
+                </div>
+                <div class="form-group">
+                    <label id="newUserNameLabel" for="newUserNameInput">Enter your new email address</label>
+                    <input required v-model="newUserName" type="text" class="form-control" id="newUserNameInput">
+                </div>
+                <div class="form-group">
+                    <label id="oldPasswordLabel" for="oldPasswordInput">Enter your old password</label>
+                    <input required v-model="oldPassword" type="text" class="form-control" id="oldPasswordInput">
+                </div>
+                <div class="form-group">
+                    <label id="newPasswordLabel" for="newPasswordInput">Enter your old password</label>
+                    <input required v-model="newPassword" type="text" class="form-control" id="newPasswordInput">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+    
+    `
+    );
 });
---
+++ src/javascript/student.js
@@ -0,0 +1,63 @@
+
+Vue.component('theTest',{
+    template:`
+   <span class="module">
+                    <legend class="col-form-legend col-sm-10"></legend>
+                    <div class="input-group input-group">
+                        <label class="wQuestionLabel" class="form-control-label" style="width: 578px;" for="wQuestion">Question:</label>
+
+                    </div>
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                         <!-- The Question Inputfield that needs ID-->
+                        
+                            <input type="text" class="form-control" aria-label="" id="${theTitle + "Q" + name}" style="width: 578px;">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                            <label id="questionOptions" class="form-control-label" style="width: 578px;"
+                                   for="wQuestion">Enter
+                                avaible options:</label>
+                        </div>
+                    </div>
+                     <!-- The radiobuttons and option inputfields that needs ID's-->
+
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                  <span class="input-group-addon">
+                    <input type="radio" value="1"  name="${theTitle + "rb" + name}" id="${theTitle + "Q" + name + "O" + "1"}" aria-label="">
+                  </span>
+                            <input type="text" id="${theTitle + "Q" + name + "input" + "1"}" class="form-control" aria-label="" style="width: 578px;">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                  <span class="input-group-addon">
+                    <input type="radio" value="2" name="${theTitle + "rb" + name}" id="${theTitle + "Q" + name + "O" + "2"}" aria-label="">
+                  </span>
+                            <input type="text" id="${theTitle + "Q" + name + "input" + "2"}" class="form-control" aria-label="" style="width: 578px;">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                  <span class="input-group-addon">
+                    <input type="radio" value="3" name="${theTitle + "rb" + name}" id="${theTitle + "Q" + name + "O" + "3"}" aria-label="">
+                  </span>
+                            <input type="text" id="${theTitle + "Q" + name + "input" + "3"}" class="form-control" aria-label="" style="width: 578px;">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="input-group input-group">
+                  <span class="input-group-addon">
+                    <input type="radio" value="4" name="${theTitle + "rb" + name}" id="${theTitle + "Q" + name + "O" + "4"}" aria-label="">
+                  </span>
+                            <input type="text" id="${theTitle + "Q" + name + "input" + "4"}" class="form-control" aria-label="" style="width: 578px;">
+                        </div>
+                    </div>
+                </span>      
+ 
+   `,
+});
